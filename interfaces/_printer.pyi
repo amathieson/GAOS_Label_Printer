2de487ce2dd95b15ab984c4521886f4dee101666
Adam Mathieson <adam@mathieson.dev>
2024-06-18 00:00:35 +0100

    + Initial Commit

---
+++ interfaces/_printer.pyi
@@ -0,0 +1,21 @@
+from structs.LabelLayout import LabelLayout
+
+
+class _PrinterInterface:
+    def get_name(self) -> str:
+        ...
+
+    def get_layouts(self) -> [LabelLayout]:
+        ...
+
+    def get_additional_methods(self) -> [str]:
+        ...
+
+    def get_additional_parameters(self) -> [(str, str)]:
+        ...
+
+    def print(self, args: object) -> None:
+        ...
+
+    def call_method(self, method_name: str):
+        ...
---
+++ structs/LabelLayout.py
@@ -0,0 +1,8 @@
+class LabelLayout:
+    ...
+
+# A Layout name
+# A Method for rendering this layout
+# Fetch MetaData about this method
+# Rendering should be able to be returned as a base64 string or a BMP Bin
+# Specify any incoming arguments
